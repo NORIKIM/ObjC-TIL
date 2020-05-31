83a906b41ad56e886413cbd37e2664f0ae0393b3
김지나 <jinagold.k@gmail.com>
2020-05-31 13:01:46 +0900

    Create musicPlayer

---
+++ MusicPlayer/MusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by 김지나 on 2020/05/31.
+//  Copyright © 2020 김지나. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
