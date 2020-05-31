a0298d4bccebc84bf97966d5476fe2bd289df133
김지나 <jinagold.k@gmail.com>
2020-05-31 13:58:51 +0900

    Connect outlet

--- MusicPlayer/MusicPlayer/ViewController.h
+++ MusicPlayer/MusicPlayer/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *playBtn;
+@property (weak, nonatomic) IBOutlet UILabel *playTimeLB;
+@property (weak, nonatomic) IBOutlet UISlider *playSlider;
 
 
 
