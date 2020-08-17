eb3f44c272827a8dd57089a0221a518fc00c51bd
김지나 <jinagold.k@gmail.com>
2020-08-17 16:52:24 +0900

    Objc to swift 기본틀 완성

---
+++ ObjcToSwift/ObjcToSwift/swiftController.swift
@@ -0,0 +1,16 @@
+//
+//  swiftController.swift
+//  ObjcToSwift
+//
+//  Created by 김지나 on 2020/08/17.
+//  Copyright © 2020 김지나. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwiftController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
